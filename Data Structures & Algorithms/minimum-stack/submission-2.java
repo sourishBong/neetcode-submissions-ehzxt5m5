9879c68eab83f2c1edb3ff24435fe1440e86506b
Sourish Mukherjee <125555099+sourishBong@users.noreply.github.com>
2026-04-26 21:02:41 +0530

    Add: minimum-stack - submission-2

---
+++ Data Structures & Algorithms/minimum-stack/submission-2.java
@@ -0,0 +1,25 @@
+class MinStack {
+        Stack<int[]> st=new Stack<>();
+       
+    public MinStack() {
+        
+    }
+    
+    public void push(int val) {
+        int currentMin = st.isEmpty() ? val : Math.min(st.peek()[1], val);
+        st.push(new int[]{val,currentMin});
+    }
+    
+    public void pop() {
+
+       if (!st.isEmpty()) st.pop();
+    }
+    
+    public int top() {
+         return st.peek()[0];
+    }
+    
+    public int getMin() {
+        return st.peek()[1];
+    }
+}
